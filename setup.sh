c5163799608459022a95ba4236a868789cf6554b
Tor Krill <tor@openproducts.se>
2019-07-10 10:38:39 +0200

    Initial commit
    Signed-off-by: Tor Krill <tor@openproducts.se>

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true
+export LC_ALL=C LANGUAGE=C LANG=C
+
+cp -a skeleton/* /
+
+dpkg -i data/kinguard-keyring_1.0_all.deb
+
+debconf-set-selections < data/kgp.preseed
+
+apt update && apt -y upgrade
+
+apt -y purge exim4 exim4-base exim4-config exim4-daemon-light
+
+apt -y install kinguard
+
+# Cleanup
+rm /etc/apt/sources.list.d/kgp-tmp.list
+
+touch /etc/opi/firstboot
+
+/usr/share/opi-postsetup/kgp-postsetup
+
+reboot
